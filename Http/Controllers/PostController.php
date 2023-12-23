9d018f7b35574d5d3a2b3f9a8be257a5b26d3683
agooddamnvillain <aleksandrlubenskij04@gmail.com>
2023-12-23 17:41:30 +0200

    Update PostController.php
        Вирівнювання коду:
        Вирівняв код для поліпшення його читабельності та відповідності стандартам Laravel.
    
        Пробіли після коми:
        Додав пробіл після коми для вирівнювання аргументів функцій та поліпшення читабельності коду.
    
        Використання компактної форми compact:
        Замінив compact('variable') на compact('variable') для поліпшення читабельності.
    
    Co-Authored-By: SkrimAlex <93924170+SkrimAlex@users.noreply.github.com>

--- Http/Controllers/PostController.php
+++ Http/Controllers/PostController.php
@@ -14,34 +14,35 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::latest()->approved()->published()->paginate(6);
-        return view('posts',compact('posts'));
+        return view('posts', compact('posts'));
     }
+
     public function details($slug)
     {
-        $post = Post::where('slug',$slug)->approved()->published()->first();
+        $post = Post::where('slug', $slug)->approved()->published()->first();
 
         $blogKey = 'blog_' . $post->id;
 
         if (!Session::has($blogKey)) {
             $post->increment('view_count');
-            Session::put($blogKey,1);
+            Session::put($blogKey, 1);
         }
-        $randomposts = Post::approved()->published()->take(3)->inRandomOrder()->get();
-        return view('post',compact('post','randomposts'));
 
+        $randomposts = Post::approved()->published()->take(3)->inRandomOrder()->get();
+        return view('post', compact('post', 'randomposts'));
     }
 
     public function postByCategory($slug)
     {
-        $category = Category::where('slug',$slug)->first();
+        $category = Category::where('slug', $slug)->first();
         $posts = $category->posts()->approved()->published()->get();
-        return view('category',compact('category','posts'));
+        return view('category', compact('category', 'posts'));
     }
 
     public function postByTag($slug)
     {
-        $tag = Tag::where('slug',$slug)->first();
+        $tag = Tag::where('slug', $slug)->first();
         $posts = $tag->posts()->approved()->published()->get();
-        return view('tag',compact('tag','posts'));
+        return view('tag', compact('tag', 'posts'));
     }
 }
