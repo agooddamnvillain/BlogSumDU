1eb09e1384a7eb70c511c26ae3285e5db873d0cd
agooddamnvillain <aleksandrlubenskij04@gmail.com>
2023-12-23 17:42:44 +0200

    Update SearchController.php
    Co-Authored-By: SkrimAlex <93924170+SkrimAlex@users.noreply.github.com>

--- Http/Controllers/SearchController.php
+++ Http/Controllers/SearchController.php
@@ -10,7 +10,12 @@ class SearchController extends Controller
     public function search(Request $request)
     {
         $query = $request->input('query');
-        $posts = Post::where('title','LIKE',"%$query%")->approved()->published()->get();
-        return view('search',compact('posts','query'));
+        
+        $posts = Post::where('title', 'LIKE', "%$query%")
+            ->approved()
+            ->published()
+            ->get();
+
+        return view('search', compact('posts', 'query'));
     }
 }
